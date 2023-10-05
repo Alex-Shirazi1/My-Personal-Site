825d87dec4f7ae346ab4aeec2dc21c995dfe41ca
Alex Shirazi <alexshirazi@Alexs-MacBook-Pro.local>
2023-10-05 15:15:46 -0700

    initial setup

---
+++ my-personal-site/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">
+            <FontAwesomeIcon icon={faHome} />
+          </Link>
+          <Link to="/about">About Me</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
