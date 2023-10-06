3f3a38d3bcfb3f8caf6dd94d6d2f4937e7e25fb1
Alex Shirazi <alexshirazi@Alexs-MacBook-Pro.local>
2023-10-05 20:31:17 -0700

    added image and styling accordingly

--- my-personal-site/src/components/Home.js
+++ my-personal-site/src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import '../App.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to My Personal Website</h1>
-            <p>intro</p>
+      <div className="home-background" style={{ backgroundImage: `url('/homeImage.jpg')` }}>
+        <div className="content">
+          <h1>Welcome to My Personal Website</h1>
+          <h1>Alex Shirazi</h1>
+          <p> ... </p>
         </div>
+      </div>
     );
-}
+  }
 
 export default Home;
